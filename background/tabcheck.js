a566003de479b44e881b923573d11a993c34870d
zek <105883248+zek-c@users.noreply.github.com>
2023-12-08 13:02:38 -0800

    add silly solution and hope it works

--- background/tabcheck.js
+++ background/tabcheck.js
@@ -1,1 +1,27 @@
+chrome.tabs.query({}, function(tabs) {
+  if (tabs.length === 1) {
+ case "CLOSE_TAB":
+    null;
+    break;
+  }
+});
 
+chrome.tabs.onCreated.addListener(function(tab) {
+  if (tabs.length=== 1) {
+    case "CLOSE_TAB":
+    null;
+    break;
+  }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  chrome.tabs.query({}, function(tabs) {
+    if (tabs.length !== 1) {
+      case "CLOSE_TAB":
+       if (currentTab && currentTab.id !== chrome.tabs.TAB_ID_NONE) {
+        chrome.tabs.remove(currentTab.id);
+      }
+      break;
+    }
+  });
+});
